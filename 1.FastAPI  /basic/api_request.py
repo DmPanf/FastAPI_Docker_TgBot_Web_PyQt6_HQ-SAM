369ab3819933c196b81a7095d7f5818f9f5a376d
Dmitrii Panfilov <99917230+DmPanf@users.noreply.github.com>
2023-08-23 13:26:35 +0300

    Create api_request.py

---
+++ 1.FastAPI  /basic/api_request.py
@@ -0,0 +1,29 @@
+import requests
+import os
+
+# Переходим в нужную директорию
+#os.chdir('/home/bunta/ARCHIVE/Projects.Python/3.Screening_System/y2.Simple.FastAPI-models/images')
+os.chdir('./images')
+print("\n\nCurrent Directory:", os.getcwd())
+
+# URL FastAPI сервера
+url = "http://0.0.0.0:8000/predict/"
+
+# Имя модели
+mdl_name = "best2.pt"
+
+# Подготовка файла
+files = {'file': ('img00.jpg', open('img00.jpg', 'rb'), 'image/jpeg')}
+
+# Отправляем POST-запрос
+response = requests.post(url, files=files, data={'mdl_name': mdl_name})
+
+# Выводим статус ответа
+print(response.status_code)
+
+# Сохраняем полученный файл, если ответ успешный
+if response.status_code == 200:
+    with open('result.jpg', 'wb') as f:
+        f.write(response.content)
+else:
+    print("Error:", response.text)
