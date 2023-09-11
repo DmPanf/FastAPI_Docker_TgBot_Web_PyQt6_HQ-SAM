4b93fdc1dedcb718f5bcb0a5f3158f6d0f5becbc
Dmitrii Panfilov <99917230+DmPanf@users.noreply.github.com>
2023-09-11 05:06:29 +0300

    Create text_module.py

---
+++ 3.TgBot  /text_module.py
@@ -0,0 +1,31 @@
+# text_module.py
+from aiogram import types
+
+# Register bot commands
+commands = [
+    types.BotCommand(command="/start", description="♻️ Запустить бота"),
+    types.BotCommand(command="/help", description="💡 Показать справку"),
+    types.BotCommand(command="/info", description="ℹ️  Информация о проекте"),
+    types.BotCommand(command="/models", description="🪩 Доступные Модели"),
+    types.BotCommand(command="/id", description="🪪 Получить свой ID"),
+    types.BotCommand(command="/list", description="📝 Список разрешенных ID"),
+    types.BotCommand(command="/add", description="🛂 Добавить ID в список"),
+    types.BotCommand(command="/del", description="🚫 Удалить ID из списка"),
+]
+
+
+help_text = """
+*Доступные команды бота:*
+
+/start - ♻️ Начало работы с ботом
+/help - 💡 Получить справку по командам
+/info - ℹ️  Информация о проекте
+/models - 🪩 Доступные Модели
+/id - 🪪 Получить свой ID
+/list - 📝 Список разрешенных ID
+/add - 🛂 Добавить ID в список Администратора
+/del - 🚫 Удалить ID из списка
+
+🖼 Отправьте изображение в обработку.
+"""
+
